d100c05810ec4b586a2ee197d3ddca5a21428791
Mark Wylde <me@markwylde.com>
2020-10-02 21:44:11 +1000

    Attempt to fix travis

--- common/database.js
+++ common/database.js
@@ -6,20 +6,39 @@ const waitOn = require('wait-on');
 
 async function connect (options) {
   console.log('Waiting for the database to be configured and running...');
-  await waitOn({
-    resources: [
-      path.resolve(options.ssl.caFile),
-      path.resolve(options.ssl.keyFile),
-      path.resolve(options.ssl.certFile),
-      `tcp:${options.host}:${options.port}`
-    ],
-    window: 1
-  });
-  console.log('Ready!');
 
-  options.ssl.ca = fs.readFileSync(path.resolve(options.ssl.caFile));
-  options.ssl.key = fs.readFileSync(path.resolve(options.ssl.keyFile));
-  options.ssl.cert = fs.readFileSync(path.resolve(options.ssl.certFile));
+  if (options.ssl) {
+    await waitOn({
+      resources: [
+        path.resolve(options.ssl.caFile),
+        path.resolve(options.ssl.keyFile),
+        path.resolve(options.ssl.certFile),
+        `tcp:${options.host}:${options.port}`
+      ],
+      window: 1
+    });
+
+    if (options.ssl.caFile) {
+      options.ssl.ca = fs.readFileSync(path.resolve(options.ssl.caFile));
+    }
+
+    if (options.ssl.keyFile) {
+      options.ssl.key = fs.readFileSync(path.resolve(options.ssl.keyFile));
+    }
+
+    if (options.ssl.certFile) {
+      options.ssl.cert = fs.readFileSync(path.resolve(options.ssl.certFile));
+    }
+  } else {
+    await waitOn({
+      resources: [
+        `tcp:${options.host}:${options.port}`
+      ],
+      window: 1
+    });
+  }
+
+  console.log('Ready');
 
   const pgp = pg(options);
 
