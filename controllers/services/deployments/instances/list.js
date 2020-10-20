dbffbfabb9031c659ffc38982771cbfff3aee240
Mark Wylde <me@markwylde.com>
2020-10-20 21:59:34 +1000

    Add deployments and services id to instance list

--- controllers/services/deployments/instances/list.js
+++ controllers/services/deployments/instances/list.js
@@ -21,7 +21,7 @@ async function listInstances ({ db, config }, request, response, tokens) {
       deploymentId: tokens.deploymentId
     },
     order: 'desc(dateCreated)',
-    fields: ['serverId', 'commitHash', 'status', 'dateCreated']
+    fields: ['serviceId', 'deploymentId', 'serverId', 'commitHash', 'status', 'dateCreated']
   });
 
   writeResponse(200, instances, response);
