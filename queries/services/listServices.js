87050d377d4e86a63b1e9e509ced6be89eafaf95
Mark Wylde <me@markwylde.com>
2020-10-30 10:08:48 +1000

    Order services by date

--- queries/services/listServices.js
+++ queries/services/listServices.js
@@ -2,7 +2,8 @@ async function listServices ({ db }, userId) {
   const services = await db.getAll('services', {
     query: {
       userId: userId
-    }
+    },
+    order: 'desc(dateCreated)'
   });
 
   return services;
